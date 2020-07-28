8d4ac649b7b299f8de8ec398375d41a110d419fb
hji92 <hji92@DESKTOP-DBKC8CG>
2020-07-28 23:33:48 +0900

    2020.07.28 AngusHwang

--- BaekJoon/src/problem14501/Main.java
+++ BaekJoon/src/problem14501/Main.java
@@ -30,9 +30,9 @@ public class Main {
 	public void dfs(int t, int p) {
 		if (T[t] <= N - t) {
 			p += P[t];
-			if(p > result) {
-				result = p;
-			}
+			
+			if(p > result) result = p;
+			
 			for(int i = t+1; i < N; i++) {
 				if(t + T[t] <= i) dfs(i, p);
 			}
